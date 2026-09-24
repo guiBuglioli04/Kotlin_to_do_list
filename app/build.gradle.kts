a8117b319b0936a1d08debcfa188e2e2aa6b7947
Guilherme Buglioli <gui_buglioli@hotmail.com>
2026-09-23 22:02:13 -0300

    ajustando build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,9 +27,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        jvmToolchain(11)
-    }
     buildFeatures {
         compose = true
     }
