a0f30afd7f27cef343fa94899a962d7b6f719168
Guilherme Buglioli <guilherme.buglioli@vtclog.com.br>
2026-09-24 12:28:32 -0300

    Finalizando alteracoes

--- app/src/main/java/guibuglioli04/com/github/todolist/MainActivity.kt
+++ app/src/main/java/guibuglioli04/com/github/todolist/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import carreiras.com.github.todolist.navigation.AppNavigation
 import carreiras.com.github.todolist.ui.theme.FiaptodolistTheme
@@ -15,10 +19,15 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FiaptodolistTheme {
-                val viewModel: TarefaViewModel = viewModel(
-                    factory = TarefaViewModel.factory(applicationContext)
-                )
-                AppNavigation(viewModel = viewModel)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val viewModel: TarefaViewModel = viewModel(
+                        factory = TarefaViewModel.factory(applicationContext)
+                    )
+                    AppNavigation(viewModel = viewModel)
+                }
             }
         }
     }
